b5bab0a4af454a9d859f7b808535959843559f23
Vsevolod <vkochamrev@gmail.com>
2018-01-28 23:16:23 +0300

    My Commit

---
+++ src/test/java/kochmarev/tests/MainTest.java
@@ -0,0 +1,12 @@
+package kochmarev.tests;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @org.junit.Test
+    public void main() throws Exception {
+        Main maintest = new Main();
+        // добавил, но сравнивать нечего
+    }
+
+}
